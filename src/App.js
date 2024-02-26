5569e4d87867134dc20751f1b1f13b8ac4264848
almayo <User@almayos-mekonen-MacBook-Air.local>
2024-02-26 12:15:45 +0200

    state added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 import Cart from './components/Cart/Cart';
+import { useState } from 'react';
 
 function App () {
+
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {}
+
+  const hideCartHandler = () => {}
+
   return (
     <CartProvider>
+      {cartIsShown && <div>Cart...</div>}
       <Cart/>
-      <Header />
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
