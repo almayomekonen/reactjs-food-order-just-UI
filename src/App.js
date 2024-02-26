8724d74e02cc7afe149695f4c0d69173cde65952
almayo <User@almayos-mekonen-MacBook-Air.local>
2024-02-26 12:43:45 +0200

    finished project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
-import Cart from './components/Cart/Cart';
 import { useState } from 'react';
 
-function App () {
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
+function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
-  const showCartHandler = () => {}
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
 
-  const hideCartHandler = () => {}
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
 
   return (
     <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
-      <Cart/>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
